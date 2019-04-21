2e7fcaffb10694ca81f82a306b0da86fabda1f96
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-22 04:11:08 +0900

    Test

--- naverMV_crowl.py
+++ naverMV_crowl.py
@@ -3,8 +3,10 @@ import requests
 import re
 import tving_crowl_func
 
+movies = []
 title_list = tving_crowl_func.getTitleList()
 for t in title_list:
+    movie = []
     t = t.replace(" ", "+")
 
     # search in Naver with movie title from Tving and Cinefox
@@ -15,7 +17,7 @@ for t in title_list:
     if html.find('ul', {'class': "search_list_1"}) != None:
         ul = html.find('ul', {'class': "search_list_1"})
         no = ul.find('a')['href'].split("/")[-1].split("=")[-1]
-        play_url = 'https://series.naver.com/tvstore/detail.nhn?mcode=' + no
+        movie.append(int(no))
 
         # get movie basic info
         url_mv = 'https://movie.naver.com/movie/bi/mi/basic.nhn?code=' + no
@@ -25,106 +27,125 @@ for t in title_list:
 
         # get title, titleEng
         title = info.find('h3', {'class': "h_movie"}).find('a').text
+        movie.append(title)
+
         titleEng = info.find('strong', {'class': "h_movie2"}).text
         titleEng = re.sub('\t|\r|\n', '', titleEng)
         rm_year = re.findall('[0-9][0-9][0-9][0-9]', titleEng)
         titleEng = titleEng.replace(', ' + rm_year[0], '')
         titleEng = titleEng.replace(rm_year[0], '')
+        movie.append(titleEng)
 
-        # get genre, nation, runtime
+        # get genre, nation, runtime, grade, opendate
         info_spec = html_mv.find('dl', {'class': 'info_spec'})
-        dd = info_spec.findAll('dd')
+        all_a_spec = info_spec.findAll('a')
 
         genre = ''
-        genre_list1 = dd[0].findAll('span')[0]
-        genre_list2 = genre_list1.findAll('a')
-        for g in genre_list2:
-            gt = g.text
-            if g == genre_list2[-1]:
-                genre += gt
-            else:
-                genre += gt + ', '
-
         nation = ''
-        nation_list1 = dd[0].findAll('span')[1]
-        nation_list2 = genre_list1.findAll('a')
-        for n in nation_list2:
-            nt = n.text
-            if n == nation_list2[-1]:
-                nation += nt
-            else:
-                nation += nt + ', '
+        runtime = ''
+        grade = ''
+        opendate = ''
 
-        runtime = dd[0].findAll('span')[2].text.strip()
+        for a in all_a_spec:
+            if 'genre' in str(a):
+                at = a.text + ', '
+                genre += at
 
-        # get opendate
-        if len(dd[0].findAll('span')) > 3:
-            year = dd[0].findAll('span')[3].findAll('a')
-            split_year1 = str(year).split('>')
-            split_year2 = split_year1[1].split('<')[0].strip()
-            split_year3 = split_year1[3].split('<')[0]
-            opendate = split_year2 + split_year3
-            opendate = opendate.replace('.', '/')
-        else:
-            opendate = ''
+            if 'nation' in str(a):
+                at = a.text + ', '
+                nation += at
 
-        # get age
-        if len(dd) > 3:
-            age = dd[3].find('a').text
-        elif len(dd) == 3:
-            age = dd[2].find('a').text
-        else:
-            age = ''
+            if 'grade' in str(a):
+                grade += a.text
+                grade = grade.replace('G', '')
+                grade = grade.replace('P-13', '')
+                grade = grade.replace('PG-13', '')
+                grade = grade.replace('P', '')
+                grade = grade.replace('PG', '')
+                grade = grade.replace('R', '')
+                grade = grade.replace('NC-17', '')
+                grade = grade.replace('N', '청소년 관람불가')
+                grade = grade.replace('NR', '등급 보류')
 
-        # get content
-        story_area = html_mv.find('div', {'class': 'story_area'})
-        head = re.findall('<h5 class="h_tx_story">(.+?)</h5>', str(story_area))
-        contents = re.findall('<p class="con_tx">(.+?)</p>', str(story_area))
-        if len(contents) >= 1:
-            contents[0] = contents[0].replace('\r', '')
-            contents[0] = contents[0].replace('\xa0', '')
-            if len(head) != 0:
-                content = head[0] + '<br/>' + contents[0]
-            else:
-                content = contents[0]
-        else:
-            content = ''
+            if 'open' in str(a):
+                at = a.text
+                opendate += at
 
-        # get poster image
-        poster = html_mv.find('div', {'class': "poster"})
-        img_src = poster.find('img')['src']
-        src = requests.get(img_src)
-        fname = no + '.jpg'
-        img_file = 'C:/stsTest/2M_Project/src/main/webapp/resources/images/' + fname
+        genre = genre[:-2]
+        movie.append(genre)
 
-        file = open(img_file, 'wb')
-        file.write(src.content)
-        file.close()
+        nation = nation[:-2]
+        movie.append(nation)
+
+        runtime_list = re.findall('<span>(.+?)분 </span>', str(info_spec))
+        if len(runtime_list) > 0:
+            runtime = runtime_list[0] + '분'
+        movie.append(runtime)
+
+        movie.append(grade)
+
+        opendate = opendate.strip()
+        opendate = opendate.replace('.', '/')
+        movie.append(opendate)
 
         # get movie detail info
         url_dt = 'https://movie.naver.com/movie/bi/mi/detail.nhn?code=' + no
         result_dt = requests.get(url_dt)
         html_dt = BeautifulSoup(result_dt.content, 'html.parser')
+        directors = html_dt.findAll('div', {'class': "dir_product"})
+        actors = html_dt.findAll('div', {'class': "p_info"})
 
         # get director list
-        directors = html_dt.find('div', {'class': "director"})
-        directors_list = re.findall('<a class="k_name" href=".+?" title=".+?">(.+?)</a>', str(directors))
         director = ''
-        for dir in directors_list:
-            if dir == directors_list[-1]:
-                director += dir
-            else:
-                director += dir + ', '
+        for dts in directors:
+            d_list = dts.findAll('a')
+            for d in d_list:
+                if 'pi/basic' in str(d):
+                    dt = d.text
+                    director += dt + ', '
+        director = director[:-2]
+        movie.append(director)
 
         # get actor list
-        actors = html_dt.find('div', {'class': "made_people"})
-        actors_list = re.findall('<a class="k_name" href=".+?" title=".+?">(.+?)</a>', str(actors))
         actor = ''
-        if len(actors_list) != 0:
-            for act in actors_list:
-                if act == actors_list[-1]:
-                    actor += act
-                else:
-                    actor += act + ', '
+        for ats in actors:
+            a_list = ats.findAll('a')
+            for ac in a_list:
+                if 'pi/basic' in str(ac):
+                    at = ac.text
+                    actor += at + ', '
+        actor = actor[:-2]
+        movie.append(actor)
 
-    # print(no, ' / ', title, ' / ', titleEng, ' / ', genre, ' / ', nation, ' / ', runtime, ' / ', age, ' / ', director, ' / ', actor, ' / ', opendate, ' / ', content, ' / ', fname, ' / ', play_url)
+        # get poster image
+        poster = html_mv.find('div', {'class': "poster"})
+        src = poster.find('img')['src']
+        img = requests.get(src)
+        fname = no + '.jpg'
+        img_file = 'C:/stsTest/2M_Project/src/main/webapp/resources/images/' + fname
+
+        file = open(img_file, 'wb')
+        file.write(img.content)
+        file.close()
+        movie.append(fname)
+
+        play_url = 'https://series.naver.com/tvstore/detail.nhn?mcode=' + no
+        movie.append(play_url)
+
+        # get content
+        story_area = html_mv.find('div', {'class': 'story_area'})
+        head = re.findall('<h5 class="h_tx_story">(.+?)</h5>', str(story_area))
+        contents = re.findall('<p class="con_tx">(.+?)</p>', str(story_area))
+        content = ''
+        if len(contents) > 0:
+            contents[0] = contents[0].replace('\r', '')
+            contents[0] = contents[0].replace('\xa0', '')
+            if len(head) > 0:
+                content = head[0] + '<br/>' + contents[0]
+            else:
+                content = contents[0]
+        movie.append(content)
+    movies.append(movie)
+
+print(movies)
+print(len(movies))
