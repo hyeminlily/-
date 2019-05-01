218f338dcacf86643b3ad2ae95e468df3c5247f5
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-05-01 18:16:39 +0900

    Test

--- save_cinefox.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import cinefox_crowl_func
-
-head = ['movie_no', 'movie_title', 'movie_titleEng', 'movie_genre', 'movie_nation', 'movie_runtime', 'movie_grade',
-        'movie_opendate', 'movie_director', 'movie_actor', 'movie_fname', 'movie_play_url', 'movie_content']
-movies = cinefox_crowl_func.getMovieList()
-print(len(movies))
-df = pd.DataFrame(movies, index=None, columns=head, dtype='int32')
-df.to_csv('movie.csv', encoding='utf-8')
