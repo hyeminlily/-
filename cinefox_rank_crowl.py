d596ce85510c2d74106994e9d9b49052e088fb6b
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-05-06 21:54:08 +0900

    Test

--- cinefox_rank_crowl.py
+++ cinefox_rank_crowl.py
@@ -6,23 +6,21 @@ import requests
 url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%B0%95%EC%8A%A4%EC%98%A4%ED%94%BC%EC%8A%A4'
 result = requests.get(url)
 html = BeautifulSoup(result.content, 'html.parser')
-div = html.findAll('strong', {'class': "scm_ellipsis_text"})
+btn_area = html.findAll('a', {'class': "btn btn_reserve"})
 
-# get Boxoffice
-boxoffice = []
-for d in div:
-    d = d.text
-    boxoffice.append(d)
-
-# get no, title from Boxoffice
+# get rank, movie_no from Boxoffice
 for i in range(10):
-    no = i + 1
-    title = boxoffice[i]
+    rank = i + 1
+
+    href = btn_area[i]['href']
+    split = href.split('=')[1]
+    movie_no = split.split('&')[0]
 
     # Oracle Connection - insert
     conn = oc.connect('hyeminseo/hyeminseo@203.236.209.97:1521/XE')
     cursor = conn.cursor()
-    cursor.execute('insert into boxoffice values(:no, :title)', no=int(no), title=str(title))
+    stmt = 'UPDATE movie SET movie_nowrank = :r WHERE movie_no = :n'
+    cursor.execute(stmt, {'r': rank, 'n': movie_no})
     conn.commit()
     cursor.close()
     conn.close
